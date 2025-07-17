240e04eeee0793d7891667a1e22d8547acfe5b08
Akash Biswas <akash41bt@gmail.com>
2025-07-17 23:34:59 +0600

    add the mcq question

---
+++ script.js
@@ -0,0 +1,31 @@
+const questions = [
+    {
+        ques: "which html tag has no closing tag?",
+        answers: [
+            {text:"<div>", correct: false},
+            {text:"<button>", correct: false},
+            {text:"<br>", correct: true},
+            {text:"<script>", correct: false},
+        ]
+    },
+    
+    {
+        ques: "which of the following is not  present of Java?",
+        answers: [
+            {text:"string", correct: false},
+            {text:"array", correct: false},
+            {text:"multimap", correct: false},
+            {text:"pointer", correct: true},
+        ]
+    },
+
+    {
+        ques: "Which of the following way is not correct to check wheather a number num is even or odd?",
+        answers: [
+            {text:"num % 2 ", correct: false},
+            {text:"num<<1 & 1", correct: true},
+            {text:"num & 1<<0", correct: false},
+            {text:"num & 1", correct: false},
+        ]
+    },
+]
